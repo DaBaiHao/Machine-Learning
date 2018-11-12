01592d47f8bf8e08a6e22d2a708e53d49d7ddae7
Hao Bai <baihaozi12@gmail.com>
2018-11-12 23:34:09 +0000

    changes

--- Assignment1-2017/part2/logistic_regression/calculate_hypothesis.m
+++ Assignment1-2017/part2/logistic_regression/calculate_hypothesis.m
@@ -2,9 +2,14 @@ function result=calculate_hypothesis(X,theta,training_example)
     hypothesis = 0.0;
     %%%%%%%%%%%%%%%%%%%%%%%%
     %Calculate the hypothesis for the i-th training example in X.
-    
+    [y , x] = size(theta);
+    for i =1 : x
+        hypothesis = hypothesis + X(training_example, i)* theta(i);
+    end
+    %hypothesis = X(training_example, 1)* theta(1) + X(training_example, 2)* theta(2) + X(training_example, 3)* theta(3) ;
     %%%%%%%%%%%%%%%%%%%%%%%%
     result=sigmoid(hypothesis);
+end
 %END OF FUNCTION
     
 
