9f618ede872f7e541cf83b1b626afc6bd03952fc
Hao Bai <baihaozi12@gmail.com>
2018-12-05 00:03:15 +0000

    c

--- Assignment2-2017/code/mog.m
+++ Assignment2-2017/code/mog.m
@@ -4,7 +4,22 @@
 % (http://www.gatsby.ucl.ac.uk/~zoubin/course02/)
 
 % Initialise parameters
-x = [f1, f2];
+[number,~] = size(phno);
+
+count_phno1 = 1;
+count_phno2 = 1;
+for i = 1:number
+    if phno(i) == 1
+        x1(count_phno1,:) = [f1(i),f2(i)];
+        count_phno1 =count_phno1 + 1;
+    end
+    
+    if phno(i) == 2
+        x2(count_phno2,:) = [f1(i),f2(i)];
+        count_phno2 =count_phno2 + 1;
+    end
+end
+x = x1;
 
 
 [n, D] = size(x);                    % number of observations (n) and dimension (D)
--- Assignment2-2017/code/task3.m
+++ Assignment2-2017/code/task3.m
@@ -1,19 +1,16 @@
 
-x = [f1, f2];
+x = x1;
 [n, D] = size(x);
 
 
 
-mean = (sum(transpose(x))')/n;    % Mean
-variance = zeros(1);              % variance
+mean = (sum(x))/n;    % Mean
+variance = zeros(1,2);              % variance
 
 k = 3;                              % number of components
 for i = 1:n
-    variance = variance + (x(i, :)- mean) * ((x(i, :) - mean)');
+    variance = variance + (x(i, :)- mean) .* ((x(i, :) - mean));
 end
 variance = variance/n;
 
-plot(x(:,1),x(:,2),'.');
-for i=1:k
-    plot_gaussian(2*s2(:,:,i),mu(:,i),i,11);
-end
+
